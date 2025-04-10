96d833d1a82569b7ac2eefe5aed01ac2bc755ad4
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-04-10 20:34:05 +0530

    feat: Create routes component with initial structure for navigation

---
+++ src/routes/routes.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+// import Home from "../pages/Home";
+// import Login from "../pages/Login";
+// import SignUp from "../pages/SignUp";
+// import Recipes from "../pages/Recipes";
+// import Recipe from "../components/Recipe";
+// import UploadRecipe from "../pages/UploadRecipe";
+// import Profile from "../pages/Profile";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export const routes:React.FC= () => {
+  return (
+    <>
+       {/* <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Recipes" element={<Recipes />} />
+        <Route path="/Recipe" element={<Recipe />} />
+        <Route path="/UploadRecipe" element={<UploadRecipe />} /> 
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter> */}
+    </>
+  )
+}
+export default routes;
